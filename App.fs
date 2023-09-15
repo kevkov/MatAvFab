3d830b79848350c444b212449009f4ebacc028fe
kev <kev.hannen@gmail.com>
2023-09-15 22:09:41 +0100

    initial commit

---
+++ App.fs
@@ -0,0 +1,103 @@
+namespace MatAvFab
+
+open System
+open Fabulous
+open Fabulous.Avalonia
+open Material.Avalonia
+
+open type Fabulous.Avalonia.View
+open Material.Colors
+open Material.Styles.Themes
+
+module App =
+    type Model =
+        { Count: int; Step: int; TimerOn: bool }
+
+    type Msg =
+        | Increment
+        | Decrement
+        | Reset
+        | SetStep of float
+        | TimerToggled of bool
+        | TimedTick
+        | TextChanged of string
+    let initModel = { Count = 0; Step = 1; TimerOn = false }
+
+    let timerCmd () =
+        async {
+            do! Async.Sleep 200
+            return TimedTick
+        }
+        |> Cmd.ofAsyncMsg
+
+    let init () =
+        FabApplication.Current.Styles.Clear()
+        FabApplication.Current.Styles.Add(MaterialTheme(null, PrimaryColor = PrimaryColor.LightBlue, SecondaryColor = SecondaryColor.Amber))
+        initModel, Cmd.none
+
+    let update msg model =
+        match msg with
+        | Increment ->
+            { model with
+                Count = model.Count + model.Step },
+            Cmd.none
+        | Decrement ->
+            { model with
+                Count = model.Count - model.Step },
+            Cmd.none
+        | Reset -> initModel, Cmd.none
+        | SetStep n -> { model with Step = int(n + 0.5) }, Cmd.none
+        | TimerToggled on -> { model with TimerOn = on }, (if on then timerCmd() else Cmd.none)
+        | TimedTick ->
+            if model.TimerOn then
+                { model with
+                    Count = model.Count + model.Step },
+                timerCmd()
+            else
+                model, Cmd.none
+        | TextChanged _ ->
+            model, Cmd.none
+
+    let view model =
+        
+        (VStack(10.) {
+            TextBlock($"%d{model.Count}").centerText()
+
+            Button("Increment", Increment).centerHorizontal()
+
+            Button("Decrement", Decrement).centerHorizontal()
+
+            (HStack() {
+                TextBlock("Timer").centerVertical()
+
+                ToggleSwitch(model.TimerOn, TimerToggled)
+            })
+                .margin(20.)
+                .centerHorizontal()
+
+            Slider(1., 10, float model.Step, SetStep)
+
+            TextBlock($"Step size: %d{model.Step}").centerText()
+
+            Button("Reset", Reset)
+                .theme("MaterialOutlineButton")
+                .classes("accent")
+                .centerHorizontal()
+            
+            TextBox("", TextChanged)
+                .theme("FilledTextBox")
+                .fontSize(10.)
+                .padding(0.)
+
+        })
+            .center()
+
+    
+#if MOBILE
+    let app model = SingleViewApplication(view model)
+#else
+    let app model = DesktopApplication(Window(view model))
+#endif
+
+    
+    let program = Program.statefulWithCmd init update app
---
+++ _StyledElement.fs
@@ -0,0 +1,30 @@
+﻿namespace MatAvFab
+
+open System.Runtime.CompilerServices
+open Avalonia
+open Avalonia.Input.TextInput
+open Avalonia.Styling
+open Fabulous
+open Fabulous.Avalonia
+
+module StyledElement =
+
+    let Theme =
+        Attributes.defineSimpleScalarWithEquality<string> "StyledElement_ThemeKey" (fun _ newValueOpt node ->
+            let target = node.Target :?> StyledElement
+
+            match newValueOpt with
+            | ValueNone -> target.Theme <- null
+            | ValueSome themeKey ->
+                let styles = Application.Current.Styles |> Seq.head :?> Styles
+
+                match styles.TryGetResource(themeKey, ThemeVariant.Default) with
+                | true, controlTheme -> target.Theme <- controlTheme :?> ControlTheme
+                | _ -> ())
+
+[<Extension>]
+type StyledElementModifiers =
+
+    [<Extension>]
+    static member inline theme(this: WidgetBuilder<'msg, #IFabStyledElement>, value: string) =
+        this.AddScalar(StyledElement.Theme.WithValue(value))
